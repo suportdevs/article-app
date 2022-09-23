6b8968d118c10fa18094aae7b4f06721ef33a368
suportdevs <suportdevs@gmail.com>
2022-09-24 00:46:22 +0600

    working on softdeletes traits

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Mamun;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, Mamun;
 
     protected $fillable = ['name', 'slug', 'description', 'created_by', 'updated_by', '_key'];
 
